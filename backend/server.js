ba4a7f7e7dbfc914c18635b8021e7cb27db92094
Rohit Chornele <rohitchornele@gmail.com>
2024-07-12 12:13:48 +0530

    fixes for deployment server changes

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/message", messageRoutes);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build', "index.html")));
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     })
